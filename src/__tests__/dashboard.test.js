84dcc86bd8259e6a753d56abe12ad18fe72bcabe
Peter Foris <peterforis6@gmail.com>
2021-08-11 13:41:35 +0200

    Correct package.json

--- src/__tests__/dashboard.test.js
+++ src/__tests__/dashboard.test.js
@@ -1,13 +1,13 @@
 /**
- * @jest-environment node
+ * @jest-environment jsdom
  */
 import Dashboard from "../components/dashboard";
 import React from "react";
 import {render} from "@testing-library/react";
 
-describe("<Dashboard />", () => {
+describe("<Dashboard /> test", () => {
     it("Renders <Dashboard /> component correctly", () => {
         const { getByText } = render(<Dashboard />);
-        expect(getByText(/List Of Episodes/i)).toBeInTheDocument();
+        expect(getByText("List Of Episodes")).toBeInTheDocument();
     });
 });
